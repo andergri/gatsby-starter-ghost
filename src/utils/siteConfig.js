afb3b6c3769ea48df1e3c5e0bc1fbbf1122dea8d
Griffin Anderson <904133+andergri@users.noreply.github.com>
2021-01-25 14:12:53 -0500

    website change

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://readforked.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://learn.readforked.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
